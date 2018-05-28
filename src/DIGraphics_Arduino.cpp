da68759fefadd903a207c6b019194fbcc6fad803
pointermess <deskbuddybeta@gmail.com>
2018-05-28 23:39:38 +0200

    added FillText and prevented inverted display

--- src/DIGraphics_Arduino.cpp
+++ src/DIGraphics_Arduino.cpp
@@ -10,8 +10,9 @@ ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
 
 void FDIGraphics_Initialize()
 {
-	
 	tft.begin();
+	tft.invertDisplay(false);
+	tft.fillScreen(ILI9341_BLACK);
 }
 
 void FDIGraphics_FillScreen()
@@ -37,4 +38,17 @@ void FDPIGraphics_DrawRectangle(unsigned int x, unsigned int y, unsigned int x2,
 void FDPIGraphics_FillRectangle(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2, unsigned short color)
 {
 	tft.fillRect(x, y, x2 - x, y2 - y, color);
+}
+#define COUNT_ENTRIES(ARRAY)    (sizeof(ARRAY) / sizeof(ARRAY[0]))
+void FDPIGraphics_FillText(char * text, unsigned int x, unsigned int y, unsigned short color, unsigned short bgcolor)
+{
+	// 1 = 6
+	// 2 = 12
+	unsigned int addX = 0;
+	tft.fillScreen(ILI9341_BLUE);
+	for (unsigned char c = 0; c < COUNT_ENTRIES(text); c++)
+	{
+		tft.drawChar(x + addX, y, text[c], ILI9341_RED, ILI9341_WHITE, 5);
+		addX += 5 * 6;
+	}
 }
--- src/DIGraphics_Arduino.h
+++ src/DIGraphics_Arduino.h
@@ -12,13 +12,13 @@
 #include <font_ArialBold.h>
 #include <font_Arial.h>
 
-
 void FDIGraphics_Initialize();
 void FDIGraphics_FillScreen();
 void FDIGraphics_DrawEllipse(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2, unsigned short color);
 void FDIGraphics_FillEllipse(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2, unsigned short color);
 void FDPIGraphics_DrawRectangle(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2, unsigned short color);
 void FDPIGraphics_FillRectangle(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2, unsigned short color);
+void FDPIGraphics_FillText(char * text, unsigned int x, unsigned int y, unsigned short color, unsigned short bgcolor);
 void FDPIGraphics_Invalidate();
 void FDIGraphics_Show();
 unsigned char FPDIGraphics_ProcessMessages();
